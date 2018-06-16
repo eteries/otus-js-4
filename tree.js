c431971131869807a10ccaef143de59039a742af
eteries <internet.practica@gmail.com>
2018-06-17 00:06:37 +0300

    requested fixes

--- tree.js
+++ tree.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 
 const tree = (root = process.argv[2] || "node_modules") => {
     const result = {files: [], folders: []};
+
+    const processFile = (file, cb) => {
+        result.files.push(file);
+        cb();
+    };
+    const processFolder = (dir, cb) => {
+        process(dir, () => {
+          result.folders.push(dir);
+          cb();
+        });
+    };
     const process = (dir, onComplete) => {
         fs.readdir(dir, (err, list) => {
             if (err) throw err;
@@ -9,22 +20,13 @@ const tree = (root = process.argv[2] || "node_modules") => {
             let queueLength = list.length;
             !queueLength && onComplete(result);
 
-            const processFile = file => {
-                result.files.push(file);
-                !--queueLength && onComplete(result);
-            };
-            const processFolder = dir => {
-                process(dir, () => {
-                    result.folders.push(dir);
-                    !--queueLength && onComplete(result);
-                });
-            };
+            const check = () => !--queueLength && onComplete(result);
 
             list.forEach(element => {
                 const path = `${dir}/${element}`;
                 fs.stat(path, (err, stats) => {
                     if (err) throw err;
-                    stats.isDirectory() ? processFolder(path) : processFile(path);
+                    stats.isDirectory() ? processFolder(path, check) : processFile(path, check);
                 });
             });
         });
